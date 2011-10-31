458abfec501aa6a51a668610e891efdbcb1e93dd
John Clarke Mills <johnclarkemills@mac.com>
2011-10-30 19:34:26 -0700

    adding twitter follow button

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,11 @@
-                <aside>
-                    <section id="logo_container">
-                    </section>
-                    <section id="video_container">
-						<div class="content-areas">
-						
+			<aside>
+				<section id="logo_container">
+				</section>
+				<section id="video_container">
+					<div class="content-areas">
+						<a href="https://twitter.com/duckpondcamp" class="twitter-follow-button" data-show-count="false">Follow @duckpondcamp</a>
+						<script src="//platform.twitter.com/widgets.js" type="text/javascript"></script>
 						<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FDuckpond%2F146594681983&amp;send=false&amp;layout=standard&amp;width=280&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=80&amp;appId=151555328193390" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:280px; height:80px;" allowTransparency="true"></iframe>
-						</div>
-                    </section>
-                </aside>
+					</div>
+				</section>
+			</aside>
